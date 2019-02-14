4e4d1996602da23b5af9682ff260117be719f889
booz24443 <booz2449@gmail.com>
2019-02-14 21:10:09 +0330

    Changed response consts

--- utils/consts.js
+++ utils/consts.js
@@ -1,14 +1,14 @@
 module.exports = {
 
-    ERR: 'Internal Error',
+    ERR: 'خطای داخلی',
 
-    INCORRECT_USER: 'Incorrect Username',
-    INCORRECT_PASSWORD : 'Incorrect Password',
-    INCORRECT_MAHTA_ID: 'Incorrect Mahta ID',
-    INCORRECT_INVITER_ID : 'Inviter Code is Incorrect',
+    INCORRECT_USER: 'نام کاربری نادرست',
+    INCORRECT_PASSWORD : 'گذرواژه نادرست',
+    INCORRECT_MAHTA_ID: 'کد مهتا نامعتبر',
+    INCORRECT_INVITER_ID : 'کد دعوت کننده نامعتبر',
 
-    MAHTA_CODE_EXISTS : 'Mahta Code is Already Taken',
-    GP_NOT_FOUND : 'No Purchase/Gift Was Found',
+    MAHTA_CODE_EXISTS : 'کد مهتا تکراری',
+    GP_NOT_FOUND : 'هیچ خرید یا هدیه ای وجود ندارد',
 
 
     SUCCESS_CODE: 200,
